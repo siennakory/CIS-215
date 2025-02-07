89bf87d781dadb66543e989678646fc92b95420b
siennakory <siennakory@gmail.com>
2025-02-06 21:48:58 -0500

    feat: updated boilerplate and completed week 2 tasks

---
+++ Week-2-Work/elseif.php
@@ -0,0 +1,13 @@
+<?php
+$x = -1;
+
+if($x > 0){
+    echo "positive";
+}
+elseif($x == 0){
+    echo "zero";
+}
+elseif($x < 0){
+    echo "negative";
+}
+?>
---
+++ Week-2-Work/siennakorytkowski.php
@@ -0,0 +1,22 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>PHP Site</title>
+    </head>
+    <body>
+
+        <header>
+            <h1>PHP Site</h1>
+            <p>This is a site I generated with PHP.</p>
+        </header>
+
+        <main>
+            <p>This site is so cool. I made it with php.</p>
+        </main>
+
+    </body>
+</html>';
+?>
