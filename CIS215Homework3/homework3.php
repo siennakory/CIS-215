ad7e7e011f769ebfaffbabe52ae049d19a47ef88
siennakory <siennakory@gmail.com>
2025-02-11 18:33:08 -0500

    feat: outlined functions for each homework task with temporary names

---
+++ CIS215Homework3/homework3.php
@@ -0,0 +1,29 @@
+<?php
+
+function main(){
+
+};
+
+function loops1(){
+
+};
+
+function loops2(){
+
+};
+
+function arrays(){
+
+};
+
+function functions(){
+
+};
+
+function fizzbuzz(){
+
+};
+
+main();
+
+?>
