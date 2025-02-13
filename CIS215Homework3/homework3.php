f46a7fda7d86cf59b1b6891dd28293829aaf39fd
siennakory <siennakory@gmail.com>
2025-02-12 19:39:43 -0500

    feat: added whileEvenArray function to generate an array of all even numbers from 1 to inputted variable ($n)

--- CIS215Homework3/homework3.php
+++ CIS215Homework3/homework3.php
@@ -6,6 +6,10 @@ function main(){
     echo " .......... ";
 
     forEchoTwoThree();
+
+    echo " .......... ";
+
+    whileEvenArray(10);
 };
 
 /*
@@ -33,7 +37,7 @@ If the number is divisible by 2 and/or 3 it will echo the number.
 A foreach type loop is used to assign the current index to a variable to be used in an if block as
 the loop iterates through the provided array.
 */
-function forEchoTwoThree(){
+function forEchoTwoThree(){ #This was the easier of the two loop types for this problem.
     $array30 = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
 
     foreach ($array30 as $num){
@@ -45,12 +49,20 @@ function forEchoTwoThree(){
     };
 };
 
-function loops2(){
+function whileEvenArray($n){
+    $count = 1;
+    $evenarray = array();
 
+    while ($count <= $n){
+        if (($count % 2) == 0){
+            array_push($evenarray, $count);
+        };
+        $count += 1;
+    };
+    var_dump($evenarray);
 };
 
-function arrays(){
-
+function forEvenArray($n){
 };
 
 function functions(){
