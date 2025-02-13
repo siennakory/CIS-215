716ddff9019292a6a1a6a0a341ecdda0e832508b
siennakory <siennakory@gmail.com>
2025-02-12 19:25:55 -0500

    style: comments added above existing functions for documentation purposes

--- CIS215Homework3/homework3.php
+++ CIS215Homework3/homework3.php
@@ -8,6 +8,11 @@ function main(){
     forEchoTwoThree();
 };
 
+/*
+whileEchoTwoThree uses a while loop to check if each number in an array is divisible by 2 and/or 3.
+If the number is divisible by 2 and/or 3 it will echo the number.
+The loop iterates through the array using the $indexcount variable to check each index.
+*/
 function whileEchoTwoThree(){
     $array30 = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
     $indexcount = 0;
@@ -22,6 +27,12 @@ function whileEchoTwoThree(){
     };
 };
 
+/*
+forEchoTwoThree uses a for loop to check if each number in an array is divisible by 2 and/or 3.
+If the number is divisible by 2 and/or 3 it will echo the number.
+A foreach type loop is used to assign the current index to a variable to be used in an if block as
+the loop iterates through the provided array.
+*/
 function forEchoTwoThree(){
     $array30 = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
 
