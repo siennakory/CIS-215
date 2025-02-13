3a383d81d218d55a11516cad65a0c49ff5a36394
siennakory <siennakory@gmail.com>
2025-02-13 17:58:17 -0500

    feat: created submission page for web form survey

---
+++ Week-4-Work/submit.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Submission Page</title>
+    </head>
+    <body>
+
+        <header>
+            <h1>Submission Confirmed</h1>
+        </header>
+
+        <main>
+            <p>Thank you for completing our survey!</p>
+        </main>
+
+    </body>
+</html>
