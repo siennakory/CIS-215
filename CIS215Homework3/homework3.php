400f5b3fe9847ec2b2d8e670686f3d594923e94d
siennakory <siennakory@gmail.com>
2025-02-12 21:33:37 -0500

    docs: added parameters and returns with typing to function comments

--- CIS215Homework3/homework3.php
+++ CIS215Homework3/homework3.php
@@ -1,5 +1,13 @@
 <?php
+/*
 
+*/
+
+/*
+main calls each function in the program. It also defines $myarray to input into the arrrayWipe function.
+Parameters: none
+Return: none
+*/
 function main(){
     whileEchoTwoThree();
 
@@ -37,6 +45,8 @@ function main(){
 whileEchoTwoThree uses a while loop to check if each number in an array is divisible by 2 and/or 3.
 If the number is divisible by 2 and/or 3 it will echo the number.
 The loop iterates through the array using the $indexcount variable to check each index.
+Parameters: none
+Return: none
 */
 function whileEchoTwoThree(){
     $array30 = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
@@ -57,6 +67,8 @@ forEchoTwoThree uses a for loop to check if each number in an array is divisible
 If the number is divisible by 2 and/or 3 it will echo the number.
 A foreach type loop is used to assign the current index to a variable to be used in an if block as
 the loop iterates through the provided array.
+Parameters: none
+Return: none
 */
 function forEchoTwoThree(){ #This was the easier of the two loop types for this problem.
     $array30 = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
@@ -74,7 +86,8 @@ function forEchoTwoThree(){ #This was the easier of the two loop types for this 
 whileEvenArray uses a while loop to generate an array of all even numbers between 1 and $n.
 It uses the $count variable to check each number until it reaches $n, using array_push to add any that are
 divisible to $evenarray. The function then uses var_dump to display the generated array for testing.
-The function returns $evenarray.
+Parameters: $n(int)
+Return: $evenarray(array)
 */
 function whileEvenArray($n){
     $count = 1;
@@ -95,7 +108,8 @@ forEvenArray uses a for loop to generate an array of all even numbers between 1 
 It uses range to generate an array of every variable from 1 to $n. It then uses a foreach
 type loop to iterate through and check each number in that array, using array_push to add any that are
 divisible by 2 to $evenarray. The function then uses var_dump to display the generated array for testing.
-The function returns $evenarray.
+Parameters: $n(int)
+Return: $evenarray(array)
 */
 function forEvenArray($n){
     $basearray = range(1, $n);
@@ -113,7 +127,8 @@ function forEvenArray($n){
 randomValues uses array_rand to randomly select two keys from an array of fruit names.
 These keys are then used to generate a second array ($endarray) with the values from the selected keys in the
 first array ($randomarray). The function then uses var_dump to display the generated array for testing.
-The function returns $endarray.
+Parameters: none
+Return: $endarray(array)
 */
 function randomValues(){
     $randomarray = array("apple", "orange", "banana", "pear", "peach");
@@ -132,7 +147,8 @@ arrayWipe removes all instances of a provided value from a provided array.
 It uses a foreach loop to iterate through each value in the provided array and check that it is
 not equal to the provided value with an if statement. If the value is not equal to the wiped value,
 it will use array_push to add the value to the new array ($wipedarray).
-This function returns $wipedarray.
+Parameters: $basearray(array), $wipe(any type in array)
+Return: $wipedarray(array)
 */
 function arrayWipe($basearray, $wipe){
     $wipedarray = array();
@@ -150,6 +166,8 @@ fizzBuzz echoes a series of numbers from 1 to $n, replacing multiples of 3 with 
 and multiples of both with FizzBuzz. $numberarray is a range of numbers from one to the inputed variable of $n.
 A foreach loop iterates through that array checking for divisibility with an if block and mod operators, and echoes
 the appropriate output.
+Parameters: $n(int)
+Return: none
 */
 function fizzBuzz($n){
     $numberarray = range(1, $n);
@@ -172,6 +190,8 @@ fizzBuzzAdapted is an altered version of the fizzBuzz function. It uses the same
 additional elseif statement is added to the if block in the foreach loop to echo Mazz if $num is divisible by 7, and
 Buzz is changed to Bozz. The purpose of this function is to demonstrate the adaptability of the original function
 by using the suggested edits with minimal edits.
+Parameters: $n(int)
+Return: none
 */
 function fizzBuzzAdapted($n){
     $numberarray = range(1, $n);
