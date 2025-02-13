9603f391a0f72d3bf78c2f4b26aee4851aa8ed96
siennakory <siennakory@gmail.com>
2025-02-12 19:07:57 -0500

    feat: added function whileEchoTwoThree to echo every number between 1 and 30 that is divisible by 2 and/or 3

--- CIS215Homework3/homework3.php
+++ CIS215Homework3/homework3.php
@@ -1,10 +1,24 @@
 <?php
 
 function main(){
-
+    whileEchoTwoThree();
 };
 
-function loops1(){
+function whileEchoTwoThree(){
+    $array30 = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
+    $indexcount = 0;
+
+    while ($indexcount < 30){
+        if (($array30[$indexcount] % 2) == 0){
+            echo $array30[$indexcount];
+        } elseif (($array30[$indexcount] % 3) == 0){
+            echo $array30[$indexcount];
+        };
+        $indexcount += 1;
+    };
+};
+
+function forEchoTwoThree(){
 
 };
 
@@ -20,7 +34,7 @@ function functions(){
 
 };
 
-function fizzbuzz(){
+function fizzBuzz(){
 
 };
 
