f14a2eb8b52033318cb2dda0c5018dac5dc16f04
siennakory <siennakory@gmail.com>
2025-02-18 03:42:03 -0500

    feat: added functions to add answers to array, determine point values from array, and print message with results also added return to form button

--- CIS215Homework4/submit-homework4.php
+++ CIS215Homework4/submit-homework4.php
@@ -13,15 +13,94 @@
 
         <main>
             <?php
-                $quiz_answers = array();
-                $count = 1;
-
-                while ($count <= 8){
-                    $answer = $_POST["question-$count"];
-                    array_push($quiz_answers, $answer);
-                    $count++;
+                
+                function main(){
+                    results(calc_points());
                 };
+
+                function calc_points(){
+                    $quiz_answers = array();
+                    $count = 1;
+
+                    while ($count <= 8){
+                        $answer = $_POST["question-$count"];
+                        array_push($quiz_answers, $answer);
+                        $count++;
+                    };
+
+                    return $quiz_answers;
+                };
+
+                function results($answers){
+                    $telepathy = 0;
+                    $strength = 0;
+                    $flying = 0;
+                    $shifter = 0;
+
+                    foreach ($answers as $a){
+                        if ($a == "Te"){
+                            $telepathy += 1;
+                        } elseif ($a == "St"){
+                            $strength += 1;
+                        } elseif ($a == "Fl"){
+                            $flying += 1;
+                        } elseif ($a == "Sh"){
+                            $shifter += 1;
+                        };
+                    };
+
+                    $options = array($telepathy, $strength, $flying, $shifter);
+
+                    rsort($options);
+
+                    $winning_value = $options[0];
+                    $winning_array = array();
+
+                    if ($telepathy == $winning_value){
+                        array_push($winning_array, "Te");
+                    };
+                    if ($strength == $winning_value){
+                        array_push($winning_array, "St");
+                    };
+                    if ($flying == $winning_value){
+                        array_push($winning_array, "Fl");
+                    };
+                    if ($shifter == $winning_value){
+                        array_push($winning_array, "Sh");
+                    };
+
+                    $count = 0;
+                    foreach ($winning_array as $value){
+                        $count++;
+                    };
+
+                    if ($count == 0){
+                        $winner = $winning_array[0];
+                        print_results($winner);
+                    } else {
+                        $count--;
+                        $winner = $winning_array[rand(0, $count)];
+                        print_results($winner);
+                    };
+                };
+
+                function print_results($winner){
+                    if ($winner == "Te"){
+                        print("<p>Your ideal superpower is TELEPATHY!</p>");
+                    } elseif ($winner == "St"){
+                        print("<p>Your ideal superpower is SUPER STRENGTH!</p>");
+                    } elseif ($winner == "Fl"){
+                        print("<p>Your ideal superpower is FLYING!</p>");
+                    } elseif ($winner == "Sh"){
+                        print("<p>Your ideal superpower is SHAPESHIFTER!</p>");
+                    };
+                };
+
+                main();
+                
             ?>
+
+            <a href="homework4.php"><button type="button" name="return" id="return">Return to Form</button></a>
         </main>
 
     </body>
