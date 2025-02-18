2114ceb89619d70f7a474bc690f508651d90d733
siennakory <siennakory@gmail.com>
2025-02-18 02:20:14 -0500

    feat: add each quiz response value to $quiz_answers array

--- CIS215Homework4/submit-homework4.php
+++ CIS215Homework4/submit-homework4.php
@@ -12,7 +12,16 @@
         </header>
 
         <main>
-            
+            <?php
+                $quiz_answers = array();
+                $count = 1;
+
+                while ($count <= 8){
+                    $answer = $_POST["question-$count"];
+                    array_push($quiz_answers, $answer);
+                    $count++;
+                };
+            ?>
         </main>
 
     </body>
