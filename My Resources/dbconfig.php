77a13d0c6fb74809434c3d7c30e812d06399d875
siennakory <siennakory@gmail.com>
2025-02-28 18:37:53 -0500

    feat: added dbconfig

---
+++ My Resources/dbconfig.php
@@ -0,0 +1,14 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+function connectDB(){
+    $database = "your username";
+    $user = "your username";
+    $pass = "your password";
+    $host = "localhost";
+    try {
+        $db = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
+        echo "db success"; }    # You’ll need to take this echo out when you know it’s working
+    catch (PDOException $e) {echo $e;}
+    return $db; } ?>
