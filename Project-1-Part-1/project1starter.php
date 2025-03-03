4d3e762b00f75dea86f6f2ecea580d0074da588a
siennakory <siennakory@gmail.com>
2025-03-03 18:02:06 -0500

    refactor: added all questions to div elements, added fieldset element for radio buttons, changed radio button label to legend

--- Project-1-Part-1/project1starter.php
+++ Project-1-Part-1/project1starter.php
@@ -8,49 +8,60 @@
     <!-- TODO: Fix all bugs/poor practice in the form -->
             <form action="" method="get" class="survey">
 
+                <div>
+                    <label>Enter your email: </label>
+                    <input type="email" name="email-name" id="email-id">
+                </div>
                 
-                <label>Enter your email: </label>
-                <input type="email" name="email-name" id="email-id">
+                <div>
+                    <label>Enter your password: </label>
+                    <input type="text" name="pw-name" id="pw-id">
+                </div>
+                
+                <div>
+                    <fieldset>
+                        <legend>What age are you? </legend>
 
-                <label>Enter your password: </label>
-                <input type="text" name="pw-name" id="pw-id">
-
-                <label>What age are you? </label>
-                <input type="radio" name="0" id="1">
-                <label>0-12 </label>
-                <input type="radio" name="1" id="1">
-                <label>13-17 </label>
-                <input type="radio" name="2" id="1">
-                <label>18-22 </label>
-                <input type="radio" name="3" id="1">
-                <label>23-27 </label>
-                <input type="radio" name="4" id="1">
-                <label>28-32 </label>
-                <input type="radio" name="5" id="1">
-                <label>33-37 </label>
-                <input type="radio" name="6" id="1">
-                <label>38-42 </label>
-                <input type="radio" name="7" id="1">
-                <label>43-47 </label>
-                <input type="radio" name="8" id="1">
-                <label>48-52 </label>
-                <input type="radio" name="9" id="1">
-                <label>53-57 </label>
-                <input type="radio" name="10" id="1">
-                <label>58-62 </label>
-                <input type="radio" name="11" id="1">
-                <label>63-67 </label>
-                <input type="radio" name="12" id="1">
-                <label>68+ </label>
-
-                <select name="gender" id="gender">
-                    <option value="m">Male</option>
-                    <option value="f">Female</option>
-                    <option value="nb">Nonbinary</option>
-                    <option value="gf">Genderfluid</option>
-                    <option value="a">Agender</option>
-                    <option value="o">Choose not to say/Other</option>
-                </select>
+                        <input type="radio" name="0" id="1">
+                        <label>0-12 </label>
+                        <input type="radio" name="1" id="1">
+                        <label>13-17 </label>
+                        <input type="radio" name="2" id="1">
+                        <label>18-22 </label>
+                        <input type="radio" name="3" id="1">
+                        <label>23-27 </label>
+                        <input type="radio" name="4" id="1">
+                        <label>28-32 </label>
+                        <input type="radio" name="5" id="1">
+                        <label>33-37 </label>
+                        <input type="radio" name="6" id="1">
+                        <label>38-42 </label>
+                        <input type="radio" name="7" id="1">
+                        <label>43-47 </label>
+                        <input type="radio" name="8" id="1">
+                        <label>48-52 </label>
+                        <input type="radio" name="9" id="1">
+                        <label>53-57 </label>
+                        <input type="radio" name="10" id="1">
+                        <label>58-62 </label>
+                        <input type="radio" name="11" id="1">
+                        <label>63-67 </label>
+                        <input type="radio" name="12" id="1">
+                        <label>68+ </label>
+                    </fieldset>
+                </div>
+                
+                <div>
+                    <select name="gender" id="gender">
+                        <option value="m">Male</option>
+                        <option value="f">Female</option>
+                        <option value="nb">Nonbinary</option>
+                        <option value="gf">Genderfluid</option>
+                        <option value="a">Agender</option>
+                        <option value="o">Choose not to say/Other</option>
+                    </select>
+                </div>
+                
 
                 <!-- TODO: Add your own survey questions -->
 
