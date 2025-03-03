edc625e280a167569233cac3acf674e50574ee29
siennakory <siennakory@gmail.com>
2025-03-03 18:23:40 -0500

    fix: separated radio buttons into divs for better styling and display

--- Project-1-Part-1/project1starter.php
+++ Project-1-Part-1/project1starter.php
@@ -22,44 +22,58 @@
                     <fieldset>
                         <legend>What age are you? </legend>
 
-                        <input type="radio" name="age" id="0-12" value="0-12">
-                        <label for="0-12">0-12 </label>
-
-                        <input type="radio" name="age" id="13-17" value="13-17">
-                        <label for="13-17">13-17 </label>
-
-                        <input type="radio" name="age" id="18-22" value="18-22">
-                        <label for="18-22">18-22 </label>
-
-                        <input type="radio" name="age" id="23-27" value="23-27">
-                        <label for="23-27">23-27 </label>
-
-                        <input type="radio" name="age" id="28-32" value="28-32">
-                        <label for="28-32">28-32 </label>
-
-                        <input type="radio" name="age" id="33-37" value="33-37">
-                        <label for="33-37">33-37 </label>
-
-                        <input type="radio" name="age" id="38-42" value="38-42">
-                        <label for="38-42">38-42 </label>
-
-                        <input type="radio" name="age" id="43-47" value="43-47">
-                        <label for="43-47">43-47 </label>
-
-                        <input type="radio" name="age" id="48-52" value="48-52">
-                        <label for="48-52">48-52 </label>
-
-                        <input type="radio" name="age" id="53-57" value="53-57">
-                        <label for="53-57">53-57 </label>
-
-                        <input type="radio" name="age" id="58-62" value="58-62">
-                        <label for="58-62">58-62 </label>
-
-                        <input type="radio" name="age" id="63-67" value="63-67">
-                        <label for="63-67">63-67 </label>
-
-                        <input type="radio" name="age" id="68+" value="68+">
-                        <label for="68+">68+ </label>
+                        <div>
+                            <input type="radio" name="age" id="0-12" value="0-12">
+                            <label for="0-12">0-12 </label>
+                        </div>
+                        <div>
+                            <input type="radio" name="age" id="13-17" value="13-17">
+                            <label for="13-17">13-17 </label>
+                        </div>
+                        <div>
+                            <input type="radio" name="age" id="18-22" value="18-22">
+                            <label for="18-22">18-22 </label>
+                        </div>
+                        <div>
+                           <input type="radio" name="age" id="23-27" value="23-27">
+                            <label for="23-27">23-27 </label> 
+                        </div>
+                        <div>
+                            <input type="radio" name="age" id="28-32" value="28-32">
+                            <label for="28-32">28-32 </label>
+                        </div>
+                        <div>
+                            <input type="radio" name="age" id="33-37" value="33-37">
+                            <label for="33-37">33-37 </label>
+                        </div>
+                        <div>
+                            <input type="radio" name="age" id="38-42" value="38-42">
+                            <label for="38-42">38-42 </label>
+                        </div>
+                        <div>
+                           <input type="radio" name="age" id="43-47" value="43-47">
+                            <label for="43-47">43-47 </label> 
+                        </div>
+                        <div>
+                            <input type="radio" name="age" id="48-52" value="48-52">
+                            <label for="48-52">48-52 </label>
+                        </div>
+                        <div>
+                            <input type="radio" name="age" id="53-57" value="53-57">
+                            <label for="53-57">53-57 </label> 
+                        </div>
+                        <div>
+                            <input type="radio" name="age" id="58-62" value="58-62">
+                            <label for="58-62">58-62 </label>
+                        </div>
+                        <div>
+                            <input type="radio" name="age" id="63-67" value="63-67">
+                            <label for="63-67">63-67 </label>
+                        </div>
+                        <div>
+                            <input type="radio" name="age" id="68+" value="68+">
+                            <label for="68+">68+ </label>
+                        </div>
                     </fieldset>
                 </div>
                 
