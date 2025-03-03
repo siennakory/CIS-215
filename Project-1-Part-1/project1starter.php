24fd63d09a486ff51336751ef8a6932e40506abf
siennakory <siennakory@gmail.com>
2025-03-03 17:57:16 -0500

    refactor: indented all different parts of the document for better organization

--- Project-1-Part-1/project1starter.php
+++ Project-1-Part-1/project1starter.php
@@ -1,60 +1,62 @@
 <!DOCTYPE html>
-<html><head>
-<title>Survey: Survey Name</title>  <!-- TODO: Change "Survey Name" to the topic of your survey -->
-</head>
-<body>
+<html>
+    <head>
+        <title>Survey: Survey Name</title>  <!-- TODO: Change "Survey Name" to the topic of your survey -->
+    </head>
+    <body>
+        <main>
+    <!-- TODO: Fix all bugs/poor practice in the form -->
+            <form action="" method="get" class="survey">
 
+                
+                <label>Enter your email: </label>
+                <input type="email" name="email-name" id="email-id">
 
-<!-- TODO: Fix all bugs/poor practice in the form -->
-<form action="" method="get" class="survey">
+                <label>Enter your password: </label>
+                <input type="text" name="pw-name" id="pw-id">
 
-<label>Enter your email: </label>
-<input type="email" name="email-name" id="email-id">
+                <label>What age are you? </label>
+                <input type="radio" name="0" id="1">
+                <label>0-12 </label>
+                <input type="radio" name="1" id="1">
+                <label>13-17 </label>
+                <input type="radio" name="2" id="1">
+                <label>18-22 </label>
+                <input type="radio" name="3" id="1">
+                <label>23-27 </label>
+                <input type="radio" name="4" id="1">
+                <label>28-32 </label>
+                <input type="radio" name="5" id="1">
+                <label>33-37 </label>
+                <input type="radio" name="6" id="1">
+                <label>38-42 </label>
+                <input type="radio" name="7" id="1">
+                <label>43-47 </label>
+                <input type="radio" name="8" id="1">
+                <label>48-52 </label>
+                <input type="radio" name="9" id="1">
+                <label>53-57 </label>
+                <input type="radio" name="10" id="1">
+                <label>58-62 </label>
+                <input type="radio" name="11" id="1">
+                <label>63-67 </label>
+                <input type="radio" name="12" id="1">
+                <label>68+ </label>
 
-<label>Enter your password: </label>
-<input type="text" name="pw-name" id="pw-id">
+                <select name="gender" id="gender">
+                    <option value="m">Male</option>
+                    <option value="f">Female</option>
+                    <option value="nb">Nonbinary</option>
+                    <option value="gf">Genderfluid</option>
+                    <option value="a">Agender</option>
+                    <option value="o">Choose not to say/Other</option>
+                </select>
 
-<label>What age are you? </label>
-<input type="radio" name="0" id="1">
-<label>0-12 </label>
-<input type="radio" name="1" id="1">
-<label>13-17 </label>
-<input type="radio" name="2" id="1">
-<label>18-22 </label>
-<input type="radio" name="3" id="1">
-<label>23-27 </label>
-<input type="radio" name="4" id="1">
-<label>28-32 </label>
-<input type="radio" name="5" id="1">
-<label>33-37 </label>
-<input type="radio" name="6" id="1">
-<label>38-42 </label>
-<input type="radio" name="7" id="1">
-<label>43-47 </label>
-<input type="radio" name="8" id="1">
-<label>48-52 </label>
-<input type="radio" name="9" id="1">
-<label>53-57 </label>
-<input type="radio" name="10" id="1">
-<label>58-62 </label>
-<input type="radio" name="11" id="1">
-<label>63-67 </label>
-<input type="radio" name="12" id="1">
-<label>68+ </label>
+                <!-- TODO: Add your own survey questions -->
 
-<select name="gender" id="gender">
-    <option value="m">Male</option>
-    <option value="f">Female</option>
-    <option value="nb">Nonbinary</option>
-    <option value="gf">Genderfluid</option>
-    <option value="a">Agender</option>
-    <option value="o">Choose not to say/Other</option>
-</select>
-
-<!-- TODO: Add your own survey questions -->
-
-</form>
-
+            </form>
+        </main>
 <!-- TODO: All the backend PHP/SQL stuff! (you may need a separate file for this!) -->
 
-</body></html>
+    </body>
+</html>
