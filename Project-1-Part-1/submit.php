e54e8d33549f6fbdf78831f60aa137cac422a8b9
siennakory <siennakory@gmail.com>
2025-03-03 19:39:10 -0500

    feat: created pwVerify function to verify password against password hash

--- Project-1-Part-1/submit.php
+++ Project-1-Part-1/submit.php
@@ -11,7 +11,25 @@
         <header></header>
 
         <main>
-            
+            <?php
+                function main(){
+                    pwVerify()
+                };
+                
+                /*
+                Tested Inputs:
+
+                */
+                function pwVerify(){
+                    $pw_hash = "$2y$10$7zeBUc4DvYw8ihrj6hrqte58o3wQUh5S6bxBxWceNK3Me6skN7.Qy";
+                    $password = $_POST["pw"];
+
+                    if (password_verify($password, $pwhash)){
+                        print("<p>Password is valid!</p>")
+                    };
+                };
+
+            ?>
         </main>
 
     </body>
