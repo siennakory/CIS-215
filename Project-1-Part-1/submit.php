02831df88dd5451270f8918633f04fcbcc519d34
siennakory <siennakory@gmail.com>
2025-03-03 20:17:26 -0500

    feat: tested password verification, added functions for input validation

--- Project-1-Part-1/submit.php
+++ Project-1-Part-1/submit.php
@@ -13,22 +13,70 @@
         <main>
             <?php
                 function main(){
-                    pwVerify();
+                    if (pwVerify() == True){
+                        if (validateInputs() == True){
+                            print("<p>All inputs are valid!</p>")
+                        };
+                    };
                 };
                 
                 /*
                 Tested Inputs:
-
+                actual password: needs to register as valid
+                password with different capitalization: make sure it is case sensitive
+                password with different numbers: make sure it is case sensitive
+                password with no numbers: make sure it it case sensitive
+                scrambled password: make sure it is case sensitive
+                wrong password: make sure it only works with the correct password
                 */
                 function pwVerify(){
-                    $pw_hash = "$2y$10$7zeBUc4DvYw8ihrj6hrqte58o3wQUh5S6bxBxWceNK3Me6skN7.Qy";
                     $password = $_POST["pw"];
 
-                    if (password_verify($password, $pwhash)){
-                        print("<p>Password is valid!</p>");
+                    if ((password_verify($password, "$2y$10\$wg0ugl7DeYf5zJdtRorV4.7bgIJgj83Tf4LJi1WyD9M2n3u8DX/ku")) == False){
+                        print("<p>Password is invalid. :(</p> <p>Plase return to the form and try again.</p>");
+                        return False;
                     } else {
-                        print("<p>Password is invalid. :(</p>");
+                        return True;
+                    };
+                };
+
+                function valEmail(){
+                    $email = $_POST["email"];
+                    if (str_contains("@") AND str_ends_with((".com") OR (".org"))){
+                        return True;
+                    } else {
+                        
                     }
+                };
+
+                function valPhone(){
+                    $phone = $_POST["phone"];
+                };
+
+                function valAge(){
+                    $age = $_POST["age"];
+                };
+
+                function valGender(){
+                    $gender = $_POST["gender"];
+                };
+
+                function valReview(){
+                    $review = $_POST["review"];
+                };
+
+                function valInputs(){
+                    if (valEmail() == True){
+                        if (valPhone() == True){
+                            if(valAge() == True){
+                                if (valGender() == True){
+                                    if (valReview() == True){
+                                        return True;
+                                    };
+                                };
+                            };
+                        };
+                    };
                 };
 
                 main();
