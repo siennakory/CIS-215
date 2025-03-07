fe7d423b9a74edb7ee2873553b7fdddfa06d784b
siennakory <siennakory@gmail.com>
2025-03-06 22:32:05 -0500

    feat: added data and summary pages for displaying sql data

---
+++ Project-1-Part-1/data.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <link href="STYLESHEET.css" rel="stylesheet" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Product Review Data</title>
+    </head>
+    <body>
+
+        <header>
+            <h1>Product Reviews:</h1>
+            <p><a href="summary.php">Too much? Check out a summary of our reviews!</a></p>
+        </header>
+
+        <main>
+            <?php
+
+                function main(){
+
+                };
+
+                function reviewArray(){
+
+                };
+
+                function displayReviews(){
+
+                };
+
+                main();
+
+            ?>
+            
+        </main>
+
+    </body>
+</html>
