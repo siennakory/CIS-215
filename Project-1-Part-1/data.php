77623e68fe7e6905fd362e8af54a0ff1cd705dc3
siennakory <siennakory@gmail.com>
2025-03-07 23:24:53 -0500

    feat: added displayReviews function to iterate through data and display review data

--- Project-1-Part-1/data.php
+++ Project-1-Part-1/data.php
@@ -32,7 +32,8 @@
                     foreach ($ids as $array){
                         array_push($id_array, $array["id"]);
                     };
-                    var_dump($id_array);
+                    
+                    displayReviews($id_array);
                 };
 
                 function displayReviews($id_array){
@@ -41,6 +42,33 @@
 
                     $select_all = $db-prepare('SELECT * FROM ProductReview');
                     $select_all->execute();
+
+                    foreach ($id_array as $id){
+                        $review_array = $select_all->fetch();
+                        $name = $review_array["name"];
+                        $age = $review_array["age"];
+                        $gender = $review_array["gender"];
+                        $stars = $review_array["stars"];
+                        $review = $review_array["review"];
+                        
+                        if ($gender == "m"){
+                            $gender = "Male";
+                        } elseif ($gender == "f"){
+                            $gender = "Female";
+                        } elseif ($gender == "nb"){
+                            $gender = "Nonbinary";
+                        } elseif ($gender == "gf"){
+                            $gender = "Genderfluid";
+                        } elseif ($gender == "a"){ 
+                            $gender = "Agender";
+                        } elseif ($gender == "o"){
+                            $gender = "Choose not to say/Other";
+                        };
+
+                        print("<div class='review'><p>Name: $name | Age: $age | Gender: $gender | Rating: $stars stars</p>
+                        <p>Review:</p>
+                        <p>$review</p></div>");
+                    };
 
                 };
 
