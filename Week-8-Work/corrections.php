5b3d78439f1cf9594de08e4ff21a7c9ef955567c
siennakory <siennakory@gmail.com>
2025-03-14 20:36:33 -0400

    feat: completed corrections on practice midterm

---
+++ Week-8-Work/corrections.php
@@ -0,0 +1,27 @@
+<?php
+
+/* This was used for corrections on the Practice Midterm as directed in my feedback. */
+
+$my_num = 0;
+++$my_num;
+--$my_num;
+$my_num++;
+$var = "False";
+
+if ($var == True){
+    print("True");
+};
+
+
+/*
+_ “000”
+_ “0.0”
+_ $my_num = 0;
+_++$my_num;
+_ --$my_num;
+_ $my_num++;
+_ 7*7 - 49
+_ “False”
+*/
+
+?>
